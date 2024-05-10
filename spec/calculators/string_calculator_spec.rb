38747b033ff6d73ceeda4cc587fe4aea2bae6339
akshay2408 <akshaybabar2408@gmail.com>
2024-05-10 16:52:37 +0530

    Created first test case for calculate sum

--- spec/calculators/string_calculator_spec.rb
+++ spec/calculators/string_calculator_spec.rb
@@ -1,2 +1,12 @@
+require 'string_calculator'
+
 describe StringCalculator do
+  describe '#calculate_sum' do
+    context 'when input is an empty string' do
+      it 'returns 0 for an empty string' do
+        calculator = StringCalculator.new
+        expect(calculator.calculate_sum('')).to eq(0)
+      end
+    end
+  end
 end
