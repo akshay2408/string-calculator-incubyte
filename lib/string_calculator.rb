70c77fdb497200225c1e3e214c554603e9aa9737
akshay2408 <akshaybabar2408@gmail.com>
2024-05-10 17:38:14 +0530

    fixed the calculate sum method for handling input contains custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,20 @@ class StringCalculator
 
     return 0 if string_numbers.empty?
 
-    numbers = string_numbers.split(",").map(&:to_i)
-    numbers.sum
+    delimiter, numbers = extract_delimiter(string_numbers)
+    numbers = numbers.split(delimiter)
+    numbers.sum(&:to_i)
+  end
+
+  private
+
+  def extract_delimiter(numbers)
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2], numbers[4..]
+      delimiter = Regexp.escape(delimiter)
+    else
+      delimiter = /[,\n]/
+    end
+    [delimiter, numbers]
   end
 end
